790ebcf14402178e8d0bd075d5875ccd426b78c9
karma21grg <kardolgrg1@gmail.com>
2020-02-10 12:59:41 +0545

    feedback

---
+++ foodrecipe/feedback/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Feedback
+
+# class ContactForm(forms.ModelForm):
+# 	class Meta:
+# 		model = Feedback
+# 		fields =['full_name','feedback_email','subject','comment']
+
