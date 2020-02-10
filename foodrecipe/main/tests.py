f38f53d9f49bdedf21e73a1f86c3dce8d8ac7ad7
tenzinsparkss <58834694+tenzinsparkss@users.noreply.github.com>
2020-02-10 17:29:11 +0545

    Updated new testcase

--- foodrecipe/main/tests.py
+++ foodrecipe/main/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
-from .models import Recipe
+from .models import Recipe, Comment, User, Item
 
 # Create your tests here.
+
+# Recipe model Test Case
 class RecipeTestCase:
     def setUp(self):
         Recipe.objects.create(recipe_title="tsampa", recipe_category="wheat")
@@ -13,4 +15,16 @@ class RecipeTestCase:
         self.assertEqual(tsampa.food(), 'This tsampa made of "wheat"')
         self.assertEqual(yaksha.food(), 'This yaksha made of "meat"')
 
+# Comment model Test Case
+class CommentTestCase:
+    def setUp(self):
+        u_obj = User.objects.create(username = "Tenzin", email = "tenzin@gmail.com")
+        # i_obj = Item.objects.create(item_title = "Momos")
+        c_obj = Comment.objects.create(msg = "beautiful", commented_by = "u_obj", item = "i_obj")
+
+    def test_comment(self):
+        user = User.objects.get(username = "Tenzin")
+        comment = Comment.objects.get(msg= "beautiful")
+        self.assertEqual(user.username, "Tenzin")
+        self.assertEqual(comment.msg, "beautiful")
 
